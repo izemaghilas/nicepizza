586c9007af533655fbbad2b13da9bbf1f1b1b30f
izemaghilas <saidtala23@gmail.com>
2021-12-26 19:20:08 +0100

    order status notifier

--- src/Controller/TrackOrderController.php
+++ src/Controller/TrackOrderController.php
@@ -3,26 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class TrackOrderController extends AbstractController
 {
     #[Route('/track-order', name: 'track_order', methods: ['GET'])]
-    public function index(Request $request, HttpClientInterface $client): Response
+    public function index(): Response
     {
+        /*
+            $orderId = $request->query->get('id');
 
-        $orderId = $request->query->get('id');
-
-        // ask notifier service to send notifications
-        $client->request('POST', $this->getParameter('app.notifier_service'), [
-            'json' => ['id' => $orderId] 
-        ]);
-
-        return $this->render('track_order/index.html.twig', [
-            'id' => $orderId,
-        ]);
+            // ask notifier service to send notifications
+            $client->request('POST', $this->getParameter('app.notifier_service'), [
+                'json' => ['id' => $orderId] 
+            ]);
+        */
+        return $this->render('track_order/index.html.twig');
     }
 }
